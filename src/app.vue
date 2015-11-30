8f81cd0b01b381593e4d46f5a904970955544644
Fanarito <viktor.saevars@gmail.com>
2015-11-30 14:39:41 +0000

    menu is now a component

--- src/app.vue
+++ src/app.vue
@@ -1,44 +1,18 @@
 <template>
-  <div class="ui top attached menu">
-    <div class="ui dropdown item">
-      <div class="text">{{dropdownText}}</div>
-      <i class="dropdown icon"></i>
-      <div class="menu">
-        <div class="item" v-link="{ path: '/' }">
-          Home
-        </div>
-        <div class="item" v-link="{ path: '/movies' }">
-          Movies
-        </div>
-        <div class="item" v-link="{ path: '/shows' }">
-          TV Shows
-        </div>
-      </div>
-    </div>
-    <div class="right menu">
-      <div class="ui right aligned category search item">
-        <div class="ui transparent icon input">
-          <input class="prompt" type="text" placeholder="Search...">
-          <i class="search link icon"></i>
-        </div>
-        <div class="results"></div>
-      </div>
-    </div>
-  </div>
+  <menu dropdownText="Home">
+  </menu>
   <div class="ui bottom attached segment">
     <router-view
       keep-alive
-      transition
-      transition-mode="out-in"
-
-
     ></router-view>
   </div>
 </template>
 
 <script>
-  var $ = window.jQuery;
   export default {
+    components: {
+      menu: require('./components/menu')
+    },
     data() {
       return {
         dropdownText: 'Home'
@@ -48,8 +22,7 @@
 
       },
       ready() {
-        $('.ui.dropdown')
-          .dropdown();
+        
       }
   }
 </script>
---
+++ src/components/menu.vue
@@ -0,0 +1,59 @@
+<style scoped>
+
+</style>
+
+<template>
+  <div class="ui top attached menu">
+    <div class="ui dropdown item">
+      <div class="text">{{menuText}}</div>
+      <i class="dropdown icon"></i>
+      <div class="menu">
+        <div class="item" v-link="{ path: '/' }">
+          Home
+        </div>
+        <div class="item" v-link="{ path: '/movies' }">
+          Movies
+        </div>
+        <div class="item" v-link="{ path: '/shows' }">
+          TV Shows
+        </div>
+      </div>
+    </div>
+    <div class="right menu">
+      <div class="ui right aligned category search item">
+        <div class="ui transparent icon input">
+          <input class="prompt" type="text" placeholder="Search...">
+          <i class="search link icon"></i>
+        </div>
+        <div class="results"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  var $ = window.jQuery;
+  export default {
+    data() {
+      return {
+        menuText: "Home"
+      }
+    },
+    ready() {
+      var path = this.$route.path;
+      path = path.split('/')
+      console.log(path);
+
+      if (path[1] === "shows") {
+        $('.ui.dropdown')
+          .dropdown('set selected', "TV Shows");
+      } else if (path[1] === "movies") {
+        $('.ui.dropdown')
+          .dropdown('set selected', "Movies");
+      } else {
+        $('.ui.dropdown')
+          .dropdown('set selected', "Home");
+      }
+    }
+  }
+</script>
