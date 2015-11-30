e92c997ed36fcaf9b627e0fafa430711eedfd99e
Fanarito <viktor.saevars@gmail.com>
2015-11-30 17:31:24 +0000

    Added season view

---
+++ src/components/season_card.vue
@@ -0,0 +1,29 @@
+<style scoped>
+
+</style>
+
+<template>
+  <div class="ui link card">
+    <div class="content">
+      <div class="header">{{season}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  var $ = window.jQuery;
+  export default {
+    props: {
+      season: {
+        type: Number
+      }
+    },
+    methods: {
+
+    },
+    computed: {},
+    ready() {
+
+    }
+  }
+</script>
--- src/views/showDetail.vue
+++ src/views/showDetail.vue
@@ -2,6 +2,11 @@
   .ui.active.inverted.dimmer {
     height: 500px;
   }
+
+  .ui.active.inverted.dimmer.episodes {
+    height: 300px;
+    margin-top: 15px;
+  }
 </style>
 
 <template>
@@ -19,6 +24,19 @@
           {{info.summary}}
         </p>
       </div>
+      <div class="four wide column">
+        <div class="ui large divided selection list">
+          <div v-for="season in seasons" @click="getEpisodes(season.season, $event)" class="item">Season {{ season.season }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div v-if="episodesLoading" class="ui active inverted dimmer episodes">
+        <div class="ui medium text loader">Loading</div>
+      </div>
+      <div v-show="selectedSeason">
+        <p v-for="episode in episodes" track_by="id">{{episode.name}}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -27,16 +45,45 @@
   var $ = window.jQuery;
   export default {
     components: {
-
+      seasoncard: require('../components/season_card')
     },
     data() {
       return {
-        loading: true
+        showId: this.$route.params.id,
+        loading: true,
+        episodesLoading: false,
+        selectedSeason: false,
+        episodes: []
+      }
+    },
+    methods: {
+      getEpisodes: function(season, e) {
+        $('.ui.large.divided.selection.list > .item').removeClass('active');
+        $(e.target).toggleClass('active');
+        this.episodesLoading = true;
+
+        var seasons = this.seasons.length;
+        var $this = this;
+
+        this.episodes = [];
+
+        $.getJSON('http://django.fanarito.duckdns.org/api/show/' + this.showId + '/' + season, {
+          format: 'json'
+        }, function(json, textStatus) {
+          // $this.episodes = [];
+          json.forEach(function(element, index){
+            $this.episodes.push(element);
+          });
+          $this.selectedSeason = true;
+          $this.episodesLoading = false;
+          console.log($this.episodes);
+        });
       }
     },
     attached() {
       var $this = this;
-      $.getJSON('http://django.fanarito.duckdns.org/api/show/' + this.$route.params.id, {
+      // Get show info
+      $.getJSON('http://django.fanarito.duckdns.org/api/show/' + this.showId, {
         format: 'json'
       }, function(json, textStatus) {
         $this.info = json.data.info;
